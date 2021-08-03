ac984991a64cf1743dd0a453b6910c72bab97653
prockwood <peter.rockwood@gmail.com>
2021-08-03 00:05:03 -0700

    adding randomize func to helper_functions.py

--- helper_function.py
+++ helper_function.py
@@ -6,7 +6,13 @@ def null_count(df):
 
 def train_test_split(df, frac):
     choice_len = round(len(df) * frac)
-    train_index = np.random.choice(len(df), choice_len, replace=False) 
+    train_index = np.random.choice(len(df), choice_len, replace=False)
     test_index = list(set(np.arange(len(df))) - set(train_index))
-
     return (df.iloc[train_index], df.iloc[test_index])
+
+def randomize(df, seed):
+    np.random.seed(seed)
+    flat_df = df.to_numpy().flatten()
+    np.random.shuffle(flat_df)
+    re_rect = flat_df.reshape(df.shape)
+    return pd.DataFrame(data=re_rect, columns=df.columns)
